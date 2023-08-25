4425186075b2e1534b1db452713712b46ea95c46
numanabid27 <numanabid27@gmail.com>
2023-08-25 18:30:31 +0500

    add pagination

--- src/Components/blog/blogs/Blogs.js
+++ src/Components/blog/blogs/Blogs.js
@@ -2,37 +2,43 @@ import React from 'react';
 import data from "../../data/data.json";
 import style from "./blog.module.css";
 import { Link } from 'react-router-dom';
+import Pagination from '@mui/material/Pagination';
+
 
 export const Blogs = () => {
-    
-  return (
-    <>
-        <section className='custom-container'>
-            <div className={style.blog_row}>
-                {
-                    data.data?.map((item)=>{
-                        return(
-                            <div key={item.id} className={style.blog_col}>
-                                <Link to={`/blog/${item.id}`}>
-                                    <img src={item.blogImag} alt='' />
-                                    <span>{item.catategry}</span>
-                                    <h2>{item.title}</h2>
-                                    <div className={style.use_row}>
-                                        <div>
-                                            <img src={item.userImg} alt="" />
+
+    return (
+        <>
+            <section className='custom-container'>
+                <div className={style.blog_row}>
+                    {
+                        data.data?.map((item) => {
+                            return (
+                                <div key={item.id} className={style.blog_col}>
+                                    <Link to={`/blog/${item.id}`}>
+                                        <img src={item.blogImag} alt='' />
+                                        <span>{item.catategry}</span>
+                                        <h2>{item.title}</h2>
+                                        <div className={style.use_row}>
+                                            <div>
+                                                <img src={item.userImg} alt="" />
+                                            </div>
+                                            <div>
+                                                <h6>{item.userName}</h6>
+                                                <p>{item.designation}</p>
+                                            </div>
                                         </div>
-                                        <div>
-                                            <h6>{item.userName}</h6>
-                                            <p>{item.designation}</p>
-                                        </div>
-                                    </div>
-                                </Link>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </section>
-    </>
-  )
+                                    </Link>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+                <div className={style.pagination}>
+                    <Pagination count={10} variant="outlined" shape="rounded" />
+                </div>
+            </section>
+        </>
+    )
 }
